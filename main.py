298fa7a252e48af544e39da93de65bc909cd9423
Md Shafaullah <mdshafaullah@intugine.com>
2023-05-22 12:18:08 +0530

    inital commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, File, UploadFile
+import shutil
+import pytesseract
+
+app = FastAPI()
+
+@app.post('/ocr')
+def ocr(image: UploadFile = File(...)):
+    filepath = 'txtFile'
+    with open(filepath, "w+b") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    return pytesseract.image_to_string(filepath, lang='eng')
